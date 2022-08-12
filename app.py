e7727ee3a2bdc28f1b97ba9db70c72af884af4bf
Bruce Herve <35220130+bruceherve@users.noreply.github.com>
2022-08-12 22:04:25 +0200

    updated greeting message

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return 'Welcome to GitOps with Kubernetes and Jenkins'
